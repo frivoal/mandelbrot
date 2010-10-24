bbf73e8822b5a7d6cc76de3e748bf54af31dd3b2
Florian Rivoal <frivoal@gmail.com>
2010-10-24 14:20:07 +0900

    Initial work on displaying in the C version

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <gtk/gtk.h>
 
-bool escape(double x, double y, int limit, int * rank)
+unsigned int escape(double x, double y, int limit)
 {
-	double z_x = 0,
-	       z_y = 0,
-	       z_x2 = 0,
-	       z_y2 = 0;
+	double z_x = 0;
+	double z_y = 0;
+	double z_x2;
+	double z_y2;
 	int i = 0;
 
 	do {
-		if (i == limit)
-			return false;
-		i++;
+		if (i++ == limit)
+			return 0;
 		z_x2 = z_x * z_x;
 		z_y2 = z_y * z_y;
-		z_x = z_x2 - z_y2 + x;
 		z_y = 2 * z_x *z_y + y;
+		z_x = z_x2 - z_y2 + x;
 
 	} while (z_x2 + z_y2 <= 4);
-	if (rank)
-		*rank = i;
-	return true;
+	return i;
 }
 
 struct converter {
@@ -64,20 +62,76 @@ double conv_y( struct converter *c, unsigned int y)
 }
 
 
-int main()
+void destroy(GtkWidget * widget, gpointer data)
 {
-	struct converter c;
-	init_converter( &c, 1024, 768, -2.5, 1, -1.5, 1.5);
+	gtk_main_quit();
+}
 
-	printf("start\n");
-	for (unsigned int y = 0; y < 1024; y++)
+unsigned int index_clip(unsigned int i,unsigned int max)
+{
+	if (!i)
+		return 0;
+	else
+		return (i - 1) % max + 1;
+}
+
+gboolean paint( GtkWidget * widget, GdkEventExpose * event, gpointer data )
+{
+	GdkGC *gcs[2];
+	gcs[0] = gdk_gc_new( widget->window );
+	gcs[1] = gdk_gc_new( widget->window );
+	GdkColor color;
+	color.red = 0;
+	color.green = 0;
+	color.blue = 0;
+	gdk_gc_set_rgb_fg_color( gcs[0], &color );
+	color.red = 0xFFFF;
+	color.green = 0xFFFF;
+	color.blue = 0xFFFF;
+	gdk_gc_set_rgb_fg_color( gcs[1], &color );
+
+	int w = widget->allocation.width;
+	int h = widget->allocation.height;
+
+	struct converter c;
+	init_converter(&c, w, h, -2.5, 1, -1.5, 1.5);
+	double *xs = g_malloc(sizeof(double)*w);
+	double *ys = g_malloc(sizeof(double)*h);
+	for (int x = 0; x < w; x++)
+		xs[x] = conv_x(&c, x);
+	for (int y = 0; y < h; y++)
+		ys[y] = conv_y(&c, y);
+	for (int y = 0; y < h; y++)
 	{
-		for(unsigned int x = 0; x < 768; x++)
+		for (int x = 0; x < w; x++)
 		{
-			escape(conv_x(&c, x), conv_y(&c, y), 1000, NULL);
+			gdk_draw_point(widget->window, gcs[index_clip(escape(xs[x], ys[y], 30),1)], x, y);
 		}
 	}
+	g_free(xs);
+	g_free(ys);
 
-	printf("end\n");
+	g_object_unref(gcs[0]);
+	g_object_unref(gcs[1]);
+	return TRUE;
+}
+
+int main(int argc, char *argv[])
+{
+	gtk_init(&argc, &argv);
+
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_resize(GTK_WINDOW(window), 300, 300);
+	g_signal_connect( G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
+
+	GtkWidget *draw_area = gtk_drawing_area_new();
+	gtk_container_add( GTK_CONTAINER(window), GTK_WIDGET(draw_area) );
+	g_signal_connect( draw_area, "expose-event", G_CALLBACK(paint), NULL );
+
+	gtk_widget_show(draw_area);
+	gtk_widget_show(window);
+
+	gtk_main();
+
 	return 0;
 }
